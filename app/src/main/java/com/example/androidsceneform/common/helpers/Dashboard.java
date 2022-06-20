c9a82595d0133b36e94155125c18110d3cc03e94
MJ_Nyota <josphatnyota@gmail.com>
2022-06-20 19:47:19 +0300

    Update Chairs.java

---
+++ app/src/main/java/com/example/androidsceneform/common/helpers/Dashboard.java
@@ -0,0 +1,15 @@
+package com.example.androidsceneform.common.helpers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.androidsceneform.R;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_dashboard);
+    }
+}
